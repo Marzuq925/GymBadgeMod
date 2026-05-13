a43ca08f7518f949f744c44cc8751727dd53dd58
Marzuq925 <arick00777@gmail.com>
2026-05-13 19:47:58 -0400

    Initial commit

---
+++ src/main/java/net/marz/badgemod/MarzsGymBadgeModClient.java
@@ -0,0 +1,14 @@
+package net.marz.badgemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarzsGymBadgeModClient implements ClientModInitializer {
+
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
